6dbae25f9e2466b40eb4f92b3b27c14b2c63e8ec
cori-spells <75272892+cori-spells@users.noreply.github.com>
2021-05-13 14:08:21 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+var coll = document.getElementsByClassName("spell");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityName).style.display = "block";
+}
