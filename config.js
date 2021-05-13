7884a48135a8b4afdc93e01ca63c03eea33355c5
cori-spells <75272892+cori-spells@users.noreply.github.com>
2021-05-13 19:56:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -21,3 +21,24 @@ function openCity(cityName) {
   }
   document.getElementById(cityName).style.display = "block";
 }
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('button');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
