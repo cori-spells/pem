c77da564c5b801b57b404733c1105d3350a9b9df
cori-spells <75272892+cori-spells@users.noreply.github.com>
2021-05-13 21:37:42 -0400

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,53 @@
+var coll = document.getElementsByClassName("spell");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityName).style.display = "block";
+}
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('button');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function myFunction2(name) {
+  var x = document.getElementsByClassName("name");
+  if (document.getElementById(name).style.display === "none") {
+    document.getElementById(name).style.display = "block";
+  } else {
+    document.getElementById(name).style.display = "none";
+  }
+}
